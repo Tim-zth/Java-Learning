c6cb61996548f8abfc69f2ca4d58c8ccf86f30e4
TimothyZhangtianhan <tzha0169@student.monash.edu>
2026-07-05 16:31:45 +1000

    Add restaurant ordering system

---
+++ src/business/RestaurantMenu.java
@@ -0,0 +1,71 @@
+package business;
+
+import java.util.Scanner;
+
+public class RestaurantMenu {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        String burger = "burger";
+        String pizza = "Pizza";
+        String pasta = "Pasta";
+
+        double burgerPrice = 12.5;
+        double pizzaPrice = 18;
+        double pastaPrice = 15;
+
+        int burgerOrder = 0;
+        int pizzaOrder = 0;
+        int pastaOrder = 0;
+
+        int burgerNum = 0;
+        int pizzaNum = 0;
+        int pastaNum = 0;
+
+        int order;
+        double total;
+        String receipt;
+
+
+        System.out.println("========== Menu ==========");
+        System.out.printf("1. %-10s    $%.2f\n", burger, burgerPrice);
+        System.out.printf("2. %-10s    $%.2f\n", pizza, pizzaPrice);
+        System.out.printf("3. %-10s    $%.2f\n", pasta, pastaPrice);
+        System.out.println("What would you like to order: ");
+        order = scanner.nextInt();
+        System.out.println("How many? ");
+        if(order == 1){
+            burgerOrder = 1;
+            burgerNum = scanner.nextInt();
+        }
+        else if(order == 2){
+            pizzaOrder = 1;
+            pizzaNum = scanner.nextInt();
+        }
+        else{
+            pastaOrder = 1;
+            pastaNum = scanner.nextInt();
+        }
+        scanner.nextLine();
+
+        total = burgerNum * burgerPrice + pizzaNum * pizzaPrice + pastaNum * pastaPrice;
+        System.out.printf("That's $%.2f in total.\n", total);
+        System.out.println("Do you want receipt? [Y/N]");
+        receipt = scanner.nextLine();
+        if(receipt.equalsIgnoreCase("Y")){
+            System.out.println("========== Receipt ==========");
+            if(burgerNum > 0){
+                System.out.printf("%s * %d    $%.2f\n", burger, burgerNum, burgerNum * burgerPrice);
+            }
+            if(pizzaNum > 0){
+                System.out.printf("%s * %d    $%.2f\n", pizza, pizzaNum, pizzaNum * pizzaPrice);
+            }
+            if(pastaNum > 0){
+                System.out.printf("%s * %d    $%.2f\n", pasta, pastaNum, pastaNum * pastaPrice);
+            }
+        }
+        System.out.printf("Total :  $%.2f\n", total);
+
+        scanner.close();
+    }
+}
