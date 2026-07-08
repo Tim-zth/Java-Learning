5c1bc5356e4b51fc332710f97125e49309125b0d
TimothyZhangtianhan <131159538+Tim-zth@users.noreply.github.com>
2026-07-08 11:29:44 +1000

    Refactor login system using methods

--- src/business/LoginSystem.java
+++ src/business/LoginSystem.java
@@ -4,163 +4,194 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class LoginSystem {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
         String username = "";
         String password = "";
-        int rand1;
-        int rand2;
-        int answer;
-        int loginStatus;
-        String email;
-        String usernameEntered;
-        String passwordEntered;
-
-        //password conditions
-        boolean containsNumber = false;
-        boolean containsUppercase = false;
-        boolean containsSpecialCharacter = false;
+        int choice;
+        boolean loggedIn = false;
 
 
-        System.out.println("========== Startup Menu ==========");
-        System.out.println("1. Sign Up");
-        System.out.println("2. Sign In");
-        loginStatus = scanner.nextInt();
+        showStartupMenu();
+
+        choice = scanner.nextInt();
         scanner.nextLine();
 
-        if(loginStatus == 1){
-            System.out.println("Please enter your email: ");
-            email = scanner.nextLine();
-            username = email.substring(0,email.indexOf("@"));
+        if (choice == 1) {
+            System.out.println("========== Sign Up ==========");
+            username = getUsername(scanner);
+            password = signUp(scanner);
 
+            System.out.println("========== Sign In ==========");
 
-            while(true){
-                System.out.println("Please enter your password: ");
-                System.out.println("1. at least 8 characters");
-                System.out.println("2. contains number");
-                System.out.println("3. contains uppercase letter");
-                System.out.println("4. contains special character");
-
-                String tempPassword = scanner.nextLine();
-
-                //reset conditions
-                containsNumber = false;
-                containsUppercase = false;
-                containsSpecialCharacter = false;
-
-
-                //check if the password is at least 8 characters
-                if(tempPassword.length() < 8){
-                    System.out.println("password less than 8 characters");
-                    continue;
-                }
-
-                //check if it contains number
-
-                for(int i = 0; i < tempPassword.length(); i++){
-                    if(containsNumber){
-                        break;
-                    }
-                    switch(tempPassword.charAt(i)){
-                        case '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' -> {
-                            //System.out.println("contains number");
-                            containsNumber = true;
-                            break;
-                        }
-                    }
-                }
-                if(!containsNumber){
-                    System.out.println("password has to contain number");
-                    continue;
-                }
-
-                //check if it contains uppercase letter
-
-                for(int i = 0; i < tempPassword.length(); i++){
-                    if(containsUppercase){
-                        break;
-                    }
-                    switch(tempPassword.charAt(i)){
-                        case 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J','K', 'L', 'M', 'N',
-                             'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ->
-                        {
-                            containsUppercase = true;
-                            break;
-                        }
-                        }
-                }
-                if(!containsUppercase){
-                    System.out.println("password has to contain uppercase");
-                    continue;
-                }
-
-                //check if it contains special character
-
-                for(int i = 0; i < tempPassword.length(); i++){
-                    if(containsSpecialCharacter){
-                        break;
-                    }
-
-                    switch(tempPassword.charAt(i)){
-                        case '!', '@', '#', '$', '%', '^', '&', '*' ->
-                        {
-                            containsSpecialCharacter = true;
-                            break;
-                        }
-                    }
-                }
-                if(!containsSpecialCharacter){
-                    System.out.println("password has to contain a special character");
-                    continue;
-                }
-                // sign up successfully
-                if(containsNumber && containsUppercase && containsSpecialCharacter){
-                    password = tempPassword;
-                    System.out.println("Sign up successful!");
-                    System.out.println("Your username is: " + username);
-                    break;
-                }
-            }
-
+            loggedIn = signIn(scanner, random, username, password);
         }
-        else if(loginStatus ==2){
-            while(true) {
-
-                System.out.println("Enter your username: ");
-                usernameEntered = scanner.nextLine();
-
-                System.out.println("Enter your password: ");
-                passwordEntered = scanner.nextLine();
-
-                if (usernameEntered.equals(username) && passwordEntered.equals(password)) {
-                    rand1 = random.nextInt(1, 101);
-                    rand2 = random.nextInt(1, 101);
-                    System.out.printf("Human verification: %d + %d = ? ", rand1, rand2);
-                    answer = scanner.nextInt();
-                    scanner.nextLine();
-                    if (answer == rand1 + rand2) {
-                        System.out.println("Login successfully");
-                        break;
-                    } else {
-                        System.out.println("You are not human");
-                        break;
-                    }
-                } else {
-                    System.out.println("Wrong username or password");
-                    System.out.println("Please try again");
-                }
-            }
-
+        else if (choice == 2){
+            System.out.println("========== Sign In ==========");
+            loggedIn = signIn(scanner, random, username, password);
         }
-        else{
-            System.out.println("Invalid Input");
-        }
-
 
         scanner.close();
     }
+
+
+    //start up menu
+    static void showStartupMenu(){
+        System.out.println("========== Startup Menu ==========");
+        System.out.println("1. Sign Up");
+        System.out.println("2. Sign In");
+    }
+
+
+    // sign in/ sign up flow
+    static String signUp(Scanner scanner){
+
+        String password = "";
+
+        System.out.println("Create your password: ");
+        System.out.println("1. at least 8 characters");
+        System.out.println("2. contains number");
+        System.out.println("3. contains uppercase letter");
+        System.out.println("4. contains special character");
+
+        do {
+
+            String tempPassword = scanner.nextLine();
+            if(!checkLength(tempPassword)){
+                System.out.println("password has to be at least 8 characters");
+            }
+            if(!checkContainsNumber(tempPassword)){
+                System.out.println("password has to contain number");
+            }
+            if(!checkContainsUppercase(tempPassword)){
+                System.out.println("password has to contain uppercase letter");
+            }
+            if(!checkContainsSpecialCharacter(tempPassword)) {
+                System.out.println("password has to contain special character");
+            }
+
+            if(isValidPassword(tempPassword)){
+                password = tempPassword;
+            }
+            else{
+                System.out.println("Create your password: ");
+            }
+        }while(password.isEmpty());
+
+        return password;
+
+    }
+
+
+    static boolean signIn(Scanner scanner, Random random, String username, String password){
+
+        String usernameEntered;
+        String passwordEntered;
+        int attempts = 3;
+
+        while(attempts > 0) {
+            System.out.println("Enter your username: ");
+            usernameEntered = scanner.nextLine();
+
+            System.out.println("Enter your password: ");
+            passwordEntered = scanner.nextLine();
+
+            attempts--;
+
+            if (usernameEntered.equals(username) && passwordEntered.equals(password)) {
+
+                if (humanVerification(scanner, random)) {
+                    System.out.println("Login successfully");
+                    return true;
+                } else {
+                    System.out.println("You are not human");
+                    break;
+                }
+            } else {
+                System.out.println("Wrong username or password");
+                System.out.printf("You have %d attempts left\n", attempts);
+            }
+        }
+        return false;
+    }
+
+
+    // check if new password is valid
+    static boolean isValidPassword(String password) {
+        return checkLength(password)
+                && checkContainsNumber(password)
+                && checkContainsUppercase(password)
+                && checkContainsSpecialCharacter(password);
+    }
+
+    static boolean checkLength(String password){
+        return password.length() >= 8;
+    }
+
+    static boolean checkContainsNumber(String password) {
+        for (int i = 0; i < password.length(); i++) {
+            switch (password.charAt(i)) {
+                case '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' -> {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    static boolean checkContainsUppercase(String password){
+        for(int i = 0; i < password.length(); i++){
+            switch(password.charAt(i)){
+                case 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J','K', 'L', 'M', 'N',
+                     'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ->
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    static boolean checkContainsSpecialCharacter(String password){
+        for(int i = 0; i < password.length(); i++){
+            if (!Character.isLetterOrDigit(password.charAt(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    // human verification for sign in
+    static boolean humanVerification(Scanner scanner, Random random){
+        int humanVerificationResponse;
+        int rand1;
+        int rand2;
+
+        rand1 = random.nextInt(1, 101);
+        rand2 = random.nextInt(1, 101);
+        System.out.printf("Human verification: %d + %d = ? ", rand1, rand2);
+        humanVerificationResponse = scanner.nextInt();
+        scanner.nextLine();
+
+        return humanVerificationResponse == rand1 + rand2;
+    }
+
+
+    // get username
+    static String getUsername(Scanner scanner){
+        System.out.println("Please enter your email: ");
+        String email = scanner.nextLine();
+        String username = email.substring(0,email.indexOf("@"));
+        System.out.println("Your username is: " + username);
+        return username;
+    }
+
+
 }
 
