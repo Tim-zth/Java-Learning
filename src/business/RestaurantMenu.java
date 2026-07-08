88c8ca53f082a1615a5028dca30b7cd12081ea2b
TimothyZhangtianhan <131159538+Tim-zth@users.noreply.github.com>
2026-07-08 12:29:27 +1000

    Refactor restaurant menu using methods

--- src/business/RestaurantMenu.java
+++ src/business/RestaurantMenu.java
@@ -6,14 +6,6 @@ public class RestaurantMenu {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        String burger = "burger";
-        String pizza = "Pizza";
-        String pasta = "Pasta";
-
-        double burgerPrice = 12.5;
-        double pizzaPrice = 18;
-        double pastaPrice = 15;
-
         int burgerNum = 0;
         int pizzaNum = 0;
         int pastaNum = 0;
@@ -22,13 +14,11 @@ public class RestaurantMenu {
         double total;
         String receipt;
 
+
         while(order != 0){
 
-            System.out.println("========== Menu ==========");
-            System.out.printf("1. %-10s    $%.2f\n", burger, burgerPrice);
-            System.out.printf("2. %-10s    $%.2f\n", pizza, pizzaPrice);
-            System.out.printf("3. %-10s    $%.2f\n", pasta, pastaPrice);
-            System.out.println("0. Check Out");
+            displayMenu();
+
             System.out.println("What would you like to order: ");
             order = scanner.nextInt();
 
@@ -44,30 +34,60 @@ public class RestaurantMenu {
                 case 3 -> pastaNum += scanner.nextInt();
             }
 
-            total = burgerNum * burgerPrice + pizzaNum * pizzaPrice + pastaNum * pastaPrice;
+            total = calculateTotalPrice(burgerNum, pizzaNum, pastaNum);
             System.out.printf("That's $%.2f in total.\n", total);
 
         }
         scanner.nextLine();
 
-        total = burgerNum * burgerPrice + pizzaNum * pizzaPrice + pastaNum * pastaPrice;
+        total = calculateTotalPrice(burgerNum, pizzaNum, pastaNum);
 
         System.out.println("Do you want receipt? [Y/N]");
         receipt = scanner.nextLine();
         if(receipt.equalsIgnoreCase("Y")){
-            System.out.println("========== Receipt ==========");
-            if(burgerNum > 0){
-                System.out.printf("%s * %d    $%.2f\n", burger, burgerNum, burgerNum * burgerPrice);
-            }
-            if(pizzaNum > 0){
-                System.out.printf("%s * %d    $%.2f\n", pizza, pizzaNum, pizzaNum * pizzaPrice);
-            }
-            if(pastaNum > 0){
-                System.out.printf("%s * %d    $%.2f\n", pasta, pastaNum, pastaNum * pastaPrice);
-            }
-            System.out.printf("Total :  $%.2f\n", total);
+            displayReceipt(burgerNum, pizzaNum, pastaNum, total);
         }
 
         scanner.close();
     }
+
+    static String burger = "burger";
+    static String pizza = "Pizza";
+    static String pasta = "Pasta";
+
+    static double burgerPrice = 12.5;
+    static double pizzaPrice = 18;
+    static double pastaPrice = 15;
+
+
+    //this method displays the menu
+    static void displayMenu(){
+
+        System.out.println("========== Menu ==========");
+        System.out.printf("1. %-10s    $%.2f\n", burger, burgerPrice);
+        System.out.printf("2. %-10s    $%.2f\n", pizza, pizzaPrice);
+        System.out.printf("3. %-10s    $%.2f\n", pasta, pastaPrice);
+        System.out.println("0. Check Out");
+    }
+
+    //this method calculates the total price
+    static double calculateTotalPrice(int burgerNum, int pizzaNum, int pastaNum){
+        return burgerNum * burgerPrice + pizzaNum * pizzaPrice + pastaNum * pastaPrice;
+    }
+
+    //this method displays the receipt
+    static void displayReceipt(int burgerNum, int pizzaNum, int pastaNum, double total){
+        System.out.println("========== Receipt ==========");
+        if(burgerNum > 0){
+            System.out.printf("%s * %d    $%.2f\n", burger, burgerNum, burgerNum * burgerPrice);
+        }
+        if(pizzaNum > 0){
+            System.out.printf("%s * %d    $%.2f\n", pizza, pizzaNum, pizzaNum * pizzaPrice);
+        }
+        if(pastaNum > 0){
+            System.out.printf("%s * %d    $%.2f\n", pasta, pastaNum, pastaNum * pastaPrice);
+        }
+        System.out.printf("Total :  $%.2f\n", total);
+    }
+
 }
