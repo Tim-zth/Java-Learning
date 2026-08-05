442d8d7d976527e23d7b4be1b9f57f3506479ab6
TimothyZhangtianhan <131159538+Tim-zth@users.noreply.github.com>
2026-08-05 11:44:07 +1000

    refactor: reorganize feature packages

--- src/com/timothy/database/DatabaseConnection.java
+++ src/com/timothy/database/DatabaseConnection.java
@@ -1,4 +1,4 @@
-package business;
+package com.timothy.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -6,14 +6,12 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private static final String URL =
-            "jdbc:mysql://localhost:3306/myDB";
+    private static final String URL = "jdbc:mysql://localhost:3306/myDB";
 
     private DatabaseConnection() {
     }
 
-    public static Connection getConnection()
-            throws SQLException {
+    public static Connection getConnection() throws SQLException {
 
         String username = System.getenv("DB_USER");
         String password = System.getenv("DB_PASSWORD");
